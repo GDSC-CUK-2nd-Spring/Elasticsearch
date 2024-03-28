fea3dba14564706aea1b3e3aef8ad25627b48ed0
YWSIK <yuusik818@gmail.com>
2024-03-28 22:50:35 +0900

    first commit

---
+++ src/main/java/com/elasticsearch/wayne/WayneApplication.java
@@ -0,0 +1,13 @@
+package com.elasticsearch.wayne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WayneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WayneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elasticsearch/wayne/WayneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elasticsearch.wayne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WayneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
